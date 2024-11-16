a4191aba7e062394809ed783ad3299578b024a79
AnnamTRAN <annampierretran@gmail.com>
2024-11-17 00:12:32 +0800

    📝 - FIX meta tags

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -55,7 +55,6 @@ const About = () => {
                         <p className='md:text-justify min-w-full w-min font-extralight text-sm'>
                             <Trans i18nKey='About.Description' />
                         </p>
-                        {/* //TODO: Add eng resume */}
                         <a href={language == "en" ? resume_en : resume_fr} target="_blank" className='md:self-start relative inline-flex overflow-hidden rounded-xl p-px'>
                             <span className='absolute inset-[-1000%] animate-spinCustom bg-[conic-gradient(from_90deg_at_50%_50%,#c2c2c2_0%,#505050_50%,#bebebe_100%)]' />
                             <span className='inline-flex gap-2 h-full w-full items-center justify-center rounded-[11px] bg-neutral-950 px-3 py-2 text-sm font-medium text-gray-50 backdrop-blur-3xl'>
