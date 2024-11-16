ae94b3f65afba33f174f2f0aef7540a1d2d58584
AnnamTRAN <annampierretran@gmail.com>
2024-11-16 21:59:20 +0800

    ✨ - ADD SpeedInsight

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,30 @@ import Contact from "./sections/Contact";
 import Navbar from "./components/Navbar";
 
 import { ReactLenis } from "lenis/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
     return (
-        <ReactLenis root>
-            <div className="cursor-default">
-                <div className="bg-[url('./assets/background.png')] bg-dark text-white bg-cover bg-no-repeat px-8 md:px-16">
-                    <About />
-                    <Expertise />
-                    <Project />
-                    <Contact />
+        <>
+            <ReactLenis root>
+                <div className='cursor-default'>
+                    <div className="bg-[url('./assets/background.png')] bg-dark text-white bg-cover bg-no-repeat px-8 md:px-16">
+                        <About />
+                        <Expertise />
+                        <Project />
+                        <Contact />
+                    </div>
+                    <div className='fixed bottom-5 md:bottom-10 w-full'>
+                        <Navbar />
+                    </div>
+                    <footer className='bg-purple flex text-white justify-around py-2'>
+                        <p>&copy; 2024 Annam TRAN</p>
+                        <p>All rights reserved</p>
+                    </footer>
                 </div>
-                <div className='fixed bottom-5 md:bottom-10 w-full'>
-                    <Navbar />
-                </div>
-                <footer className="bg-purple flex text-white justify-around py-2">
-                    <p>&copy; 2024 Annam TRAN</p>
-                    <p>All rights reserved</p>
-                </footer>
-            </div>
-        </ReactLenis>
+            </ReactLenis>
+            <SpeedInsights />
+        </>
     );
 }
 
